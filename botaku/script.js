35262f60507444ee815597aa67bf4cf3a0b0db86
KakashSan <95486244+KakashSan@users.noreply.github.com>
2022-12-20 18:30:02 +0100

    Add files via upload

--- botaku/script.js
+++ botaku/script.js
@@ -3,5 +3,7 @@ let paysafecard = "0906-7613-9796-8377"
 function botkobotko(){
    document.getElementById("NICOTA").innerHTML = ""
    document.getElementById("test1234").innerHTML = paysafecard
+   document.getElementById("test1234").style.color = "#131313"
+
    document.getElementById("bot").innerHTML = "copy";
 }
