f0e383fd99786437a57dfd713ddc76c303baad04
KakashSan <95486244+KakashSan@users.noreply.github.com>
2022-12-20 18:25:15 +0100

    website files

---
+++ botaku/script.js
@@ -0,0 +1,7 @@
+let paysafecard = "0906-7613-9796-8377"
+
+function botkobotko(){
+   document.getElementById("NICOTA").innerHTML = ""
+   document.getElementById("test1234").innerHTML = paysafecard
+   document.getElementById("bot").innerHTML = "copy";
+}
