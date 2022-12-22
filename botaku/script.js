8a732609e2e5a28ecaa8157633950fd415dc12f2
KakashSan <95486244+KakashSan@users.noreply.github.com>
2022-12-22 13:10:48 +0100

    viktorbot

--- botaku/script.js
+++ botaku/script.js
@@ -1,5 +1,16 @@
 let paysafecard = "0906-7613-9796-8377"
 
+/*var heslacek = document.getElementById("heslo").value*/
+
+function hesielko() {
+   if(document.getElementById("heslo").value=="cernamamba") {
+      botkobotko()
+   }
+   else {
+   document.getElementById("nespravne").innerHTML = "NESPRAVNE"
+   }
+}
+
 function botkobotko(){
    document.getElementById("NICOTA").innerHTML = ""
    document.getElementById("test1234").innerHTML = paysafecard
